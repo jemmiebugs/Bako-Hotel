ebe737d6e8e1fb6838f1aa80a952552d685ba1f0
Jeminat Muritala <jemmynat@yahoo.com>
2020-03-23 14:24:38 +0100

    A five star hotel with a 24/7 online presence

---
+++ js/main.js
@@ -0,0 +1,87 @@
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+       // Search model
+    $('.search-switch').on('click', function () {
+        $('.search-model').fadeIn(400);
+    });
+
+    $('.search-close-switch').on('click', function () {
+        $('.search-model').fadeOut(400, function () {
+            $('#search-input').val('');
+        });
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*------------------
+        Hero Slider
+    --------------------*/
+   $(".hero-slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: true,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        mouseDrag: false
+    });
+
+    /*------------------------
+		Testimonial Slider
+    ----------------------- */
+    $(".testimonial-slider").owlCarousel({
+        items: 1,
+        dots: false,
+        autoplay: true,
+        loop: true,
+        smartSpeed: 1200,
+        nav: true,
+        navText: ["<i class='arrow_left'></i>", "<i class='arrow_right'></i>"]
+    });
+
+    /*------------------
+        Magnific Popup
+    --------------------*/
+    $('.video-popup').magnificPopup({
+        type: 'iframe'
+    });
+
+    /*------------------
+		Date Picker
+	--------------------*/
+    $(".date-input").datepicker({
+        minDate: 0,
+        dateFormat: 'dd MM, yy'
+    });
+
+    /*------------------
+		Nice Select
+	--------------------*/
+    $("select").niceSelect();
+
+})(jQuery);
